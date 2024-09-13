3f1547d2c965c7d3c6a184e12dcee01891677af5
Akrom <akromxolboyev0615@gmail.com>
2024-09-13 18:31:24 +0500

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="container p-5">
+      <Button variant={"destructive"}>lorem</Button>
+      <Button variant={"default"}>lorem</Button>
+      <Button variant={"default"}>lorem</Button>
+      <Button variant={"secondary"}>lorem</Button>
+    </div>
+  );
+}
