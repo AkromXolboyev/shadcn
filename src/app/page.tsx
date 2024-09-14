beca13c71822f5318b6adb185811b57d1e5770ef
Akrom <akromxolboyev0615@gmail.com>
2024-09-14 07:43:17 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,94 @@
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Home() {
   return (
-    <div className="container p-5">
-      <Button variant={"destructive"}>lorem</Button>
-      <Button variant={"default"}>lorem</Button>
-      <Button variant={"default"}>lorem</Button>
-      <Button variant={"secondary"}>lorem</Button>
+    <div>
+      <div className="container p-5">
+        <Button size={"icon"} variant={"destructive"}>
+          lorem
+        </Button>
+        <Button size={"lg"} variant={"default"}>
+          lorem
+        </Button>
+        <Button variant={"default"}>lorem</Button>
+        <Button variant={"secondary"}>lorem</Button>
+      </div>
+      <div className="container mt-5 bg-slate-200">
+        <h1>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati,
+          ut voluptate quas esse error molestias! Vitae explicabo dicta totam
+          illum odio eaque quos nostrum repellendus commodi est. Rerum, magni
+          quae.
+        </h1>
+        <div className="bg-yellow-400 w-16 rounded-md p-2">
+          <Drawer>
+            <DrawerTrigger>Open</DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                <DrawerDescription>
+                  This action cannot be undone.
+                </DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      <div className="container">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>Av</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   );
 }
